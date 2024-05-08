aa4ee2146cdb615988d164a37193c9da6efcd93f
Fernando D'luccas <dluccash@hotmail.com>
2024-05-08 16:38:15 -0300

    chore: set up ESLint configuration for application structure

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import fastify from 'fastify';
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
 app.get('/', async (request, reply) => {
-  return { hello: 'world' };
-});
+  return { hello: 'world' }
+})
 
 app.listen({ port: 3333 }).then(() => {
-  console.log('HTTP Server Running!');
-});
+  console.log('HTTP Server Running!')
+})
