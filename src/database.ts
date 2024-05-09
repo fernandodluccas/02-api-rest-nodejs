2d39b1cdbd397794c634c2b63da954f24576cee1
Fernando D'luccas <dluccash@hotmail.com>
2024-05-09 13:11:33 -0300

    feat: Create initial database migration

---
+++ src/database.ts
@@ -0,0 +1,17 @@
+import { knex as setupKnex, Knex } from 'knex'
+
+export const config: Knex.Config = {
+  client: 'sqlite3',
+  connection: {
+    filename: './db/app.db',
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+}
+
+const knex = setupKnex(config)
+
+export { knex }
