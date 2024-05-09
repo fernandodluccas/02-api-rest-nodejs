9b26d7241edafbe929c2392dfb534b4c7cc31b1b
Fernando D'luccas <dluccash@hotmail.com>
2024-05-09 15:44:04 -0300

    feat: implement environment variables handling with Zod for database configuration

---
+++ src/env/index.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NODE_ENV: z
+    .enum(['development', 'production', 'test'])
+    .default('development'),
+  DATABASE_URL: z.string(),
+  PORT: z.number().default(3333),
+})
+
+export const env = envSchema.parse(process.env)
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
+import 'dotenv/config'
 import fastify from 'fastify'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
@@ -11,6 +13,6 @@ app.get('/', async () => {
   return transaction
 })
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP Server Running!')
 })
