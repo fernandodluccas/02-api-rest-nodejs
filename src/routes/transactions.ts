cc22052989f28cd481c6c14ee4d825a2e7c24c76
Fernando D'luccas <dluccash@hotmail.com>
2024-05-10 00:15:19 -0300

    feat: implement transaction creation routes

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,12 +1,26 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
+import { z } from 'zod'
+import { randomUUID } from 'node:crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const transaction = await knex('transactions')
-      .where({ amount: 1000 })
-      .select('*')
+  app.post('/', async (request, reply) => {
+    const createRansactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
 
-    return transaction
+    const { title, amount, type } = createRansactionBodySchema.parse(
+      request.body,
+    )
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.code(201).send()
   })
 }
