08e87227722f57dd2612e8026d6e090d2dec4e8f
Fernando D'luccas <dluccash@hotmail.com>
2024-05-11 17:06:38 -0300

    feat: add automated test for transaction listing

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { beforeAll, afterAll, describe, it } from 'vitest'
+import { beforeAll, afterAll, describe, it, expect } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
@@ -20,5 +20,29 @@ describe('Transaction Routes', () => {
         type: 'credit',
       })
       .expect(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Test transaction',
+        amount: 100,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.headers['set-cookie']
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'Test transaction',
+        amount: 100,
+      }),
+    ])
   })
 })
